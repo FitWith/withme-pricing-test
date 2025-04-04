2a78b11b5de15c67b8bc3bdce1721d3d2ba2a132
Jay Mistry <jay.mistry@tecocraft.com>
2025-04-04 12:21:34 +0530

    update pricing component with new discovery call link and commented out pricing display

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -281,15 +281,15 @@ export function SuccessLaunch() {
             <div className="rounded-2xl bg-gray-50 py-3 text-center ring-1 ring-inset ring-gray-900/5 lg:flex lg:flex-col lg:justify-center lg:py-6">
               <div className="mx-auto max-w-xs px-8">
                 <p className="text-base font-semibold text-gray-600">Implementation Specialist</p>
-                <p className="mt-4 flex items-baseline justify-center gap-x-2">
+                {/* <p className="mt-4 flex items-baseline justify-center gap-x-2">
                   <span className="text-4xl font-semibold tracking-tight text-gray-900">{currencySymbol}{implementationPrices[code]}</span>
                   <span className="text-sm/6 font-semibold tracking-wide text-gray-600">{code.toUpperCase()}</span>
-                </p>
+                </p> */}
                 <a
-                  href={getBuyLink(PRODUCT_IDS.implementation)} target='_blank'
+                  href={'https://calendly.com/markgray/implementation-programme?month=2024-12'} target='_blank'
                   className="mt-6 block w-full rounded-md bg-[#3C55F3] px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-[#3347d1] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#3C55F3]"
                 >
-                  Get started
+                  Discovery Call
                 </a>
               </div>
             </div>
